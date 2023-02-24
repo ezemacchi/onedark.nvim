eec016d648da1f35c8467c8eae669cdf25c143e6
ezemacchi <macchimallo@gmail.com>
2023-02-24 13:00:14 -0300

    added new custom property
    ["@type.qualifier"] = {fg = c.purple},

--- lua/onedark/highlights.lua
+++ lua/onedark/highlights.lua
@@ -193,6 +193,7 @@ if vim.api.nvim_call_function("has", { "nvim-0.8" }) == 1 then
         ["@danger"] = colors.Fg,
         ["@type"] = colors.Yellow,
         ["@type.builtin"] = colors.Orange,
+        ["@type.qualifier"] = {fg = c.purple},
         ["@variable"] = {fg = c.fg, fmt = cfg.code_style.variables},
         ["@variable.builtin"] = {fg = c.red, fmt = cfg.code_style.variables},
     }
